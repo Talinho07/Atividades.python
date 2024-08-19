dbd195dc43aa2fa9e3ad1b80525d030f694782ed
Talinho07 <nonatotalys@gmail.com>
2024-08-19 11:15:31 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,39 @@
+# Comentários de uma linha
+# Tipagem dinamica
+
+teste = "Olá"
+print(type(teste))
+teste = 30
+print(type(teste))
+teste = 2.4
+print(type(teste))
+
+nome = input("digite o seu nome: ")
+print("olá", nome)
+idade = input("digite a sua idade")
+print(idade)
+print(type(idade))
+
+#tipos de Dados
+#int : números inteiros
+#float : números reais, decimais
+#str : string, cadeia de caracteres, utilizando aspas
+#boolean : tipo lógico true ou falso
+
+#valor A = int(input("digite o valor de A:"))
+#valor B = int(input("digite o valor de B:"))
+#print(valor A + valor B)
+#print(type(valor A))
+
+#operadores aritmeticos
+
+x=3
+
+print(x+1) #Adicao
+print(x-1) #subtracao
+print(x*2) #multiplicacao
+print(x/2) #divisao
+print(x//2) #divisao inteira
+print(x ** 2) #exponenciacao
+print(x % 2) #resto da divisao/ módulo
+
